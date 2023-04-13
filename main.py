6b8e20b6ec304c31fc5b0e09a86124688997b1dc
suribe06 <santiu990506@gmail.com>
2023-04-13 14:58:03 -0500

    function to extract text from pdf with the option to join all pdfs in a txt or generate a txt for each pdf

--- main.py
+++ main.py
@@ -7,36 +7,44 @@ from nltk.corpus import stopwords
 
 stop_words = set(stopwords.words('english'))
 
-BASE_DIR = os.getcwd()
-ART_DIR = os.path.join(BASE_DIR, 'articles')
-R_DIR = os.path.join(BASE_DIR, 'R_Code')
+def extract_text_from_pdfs(merge=False):
+    BASE_DIR = os.getcwd()
+    ART_DIR = os.path.join(BASE_DIR, 'articles')
+    R_DIR = os.path.join(BASE_DIR, 'R_Code', 'articles')
+    text = ''
+    for i, file in enumerate(os.listdir(ART_DIR)):
+        # Read only PDF files
+        if not file.endswith('.pdf'):
+            continue
+        article_path = os.path.join(ART_DIR, file)
+        with open(article_path, 'rb') as f:
+            pdf = PdfReader(f)
+            article_text = ""
+            for j, page in enumerate(pdf.pages):
+                if j == 0: continue # Skip the first page
+                # Extract the text from the page
+                text = page.extract_text()
+                # Convert text to ASCII
+                text = unidecode(text)
+                # Convert all symbols to lower case
+                text = text.lower()
+                # Remove numeric characters
+                text = re.sub(r'\d+', '', text)
+                # Remove non-alphanumeric characters
+                text = re.sub(r"[^\w']+", " ", text)
+                # Replace one or more whitespace characters with a single space
+                text = re.sub(r'\s+', ' ', text)
+                # Remove stop words
+                tokens = word_tokenize(text)
+                filtered_tokens = [token for token in tokens if token not in stop_words]
+                article_text += ' '.join(filtered_tokens)
+        if merge:
+            text += article_text
+        else:
+            with open(os.path.join(R_DIR, f'article_{i}.txt'), 'w', encoding='utf-8') as text_file:
+                text_file.write(article_text)
+    if merge:
+        with open(os.path.join(R_DIR, 'combined_articles.txt'), 'w', encoding='utf-8') as text_file:
+            text_file.write(text)
 
-# Extracting the text of the PDF articles and writing to a text file
-for i, file in enumerate(os.listdir(ART_DIR)):
-    # Read only PDF files
-    if not file.endswith('.pdf'):
-        continue
-    article_path = os.path.join(ART_DIR, file)
-    with open(article_path, 'rb') as f:
-        pdf = PdfReader(f)
-        article_text = ""
-        for j, page in enumerate(pdf.pages):
-            if j == 0: continue # Skip the first page
-            # Extract the text from the page
-            text = page.extract_text()
-            # Convert text to ASCII
-            text = unidecode(text)
-            # Convert all symbols to lower case
-            text = text.lower()
-            # Remove numeric characters
-            text = re.sub(r'\d+', '', text)
-            # Remove non-alphanumeric characters
-            text = re.sub(r"[^\w']+", " ", text)
-            # Replace one or more whitespace characters with a single space
-            text = re.sub(r'\s+', ' ', text)
-            # Remove stop words
-            tokens = word_tokenize(text)
-            filtered_tokens = [token for token in tokens if token not in stop_words]
-            article_text += ' '.join(filtered_tokens)
-        with open(os.path.join(R_DIR, f'article_{i}.txt'), 'w', encoding='utf-8') as text_file:
-            text_file.write(article_text)
+extract_text_from_pdfs(merge=True)
