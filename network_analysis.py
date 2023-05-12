05f9d8595d116e92e9a848ec4f8a07a573b0f359
fca2023 <profeponganos5@gmail.com>
2023-05-12 14:03:44 -0500

    top nodes graph (degree and eigen)

--- network_analysis.py
+++ network_analysis.py
@@ -111,7 +111,7 @@ def centralityMeasureAnalysisSNAP():
     degree = np.zeros(N, dtype=float)
     # Centrality measures
     NodesBet, EdgesBet = G.GetBetweennessCentr(1.0)
-    for NI in G.Nodes():
+    for NI in nodes:
         node_id = NI.GetId()
         closeness[node_id] = G.GetClosenessCentr(node_id)
         betweenness[node_id] = NodesBet[node_id]
@@ -141,5 +141,47 @@ def centralityMeasureAnalysisSNAP():
     gitUpdate('update lattice data')
     return
 
+def getInducedSubGraph():
+    GRAPH_FILE = os.path.join(GR_DIR, f'graph_skipgrams.gml')
+    G_nx = nx.read_gml(GRAPH_FILE, label='id')
+    node_mapper = {node: G_nx.nodes[node]["name"] for node in G_nx.nodes}
+    # Convert networkx graph to snap graph
+    G = networkxToSnap(G_nx)
+    N = G.GetNodes()
+    nodes = G.Nodes()
+    # Take the n most important nodes, in this case eigenvalues are used as decision criterion
+    n = 15
+    eigenvector = np.zeros(N, dtype=float)
+    NIdEigenH = G.GetEigenVectorCentr()
+    for NI in nodes:
+        node_id = NI.GetId()
+        eigenvector[node_id] = NIdEigenH[node_id]
+    top_nodes = sorted(list(zip(range(N), eigenvector)), key=lambda x: x[1], reverse=True)[:n]
+    top_node_ids = [node[0] for node in top_nodes]
+    print(top_node_ids)
+    H = G.ConvertSubGraph(snap.TUNGraph, top_node_ids)
+    print(f'nodes {H.GetNodes()} edges {H.GetEdges()}')
+    H.SaveEdgeList('mygraph.txt')
+    names = [node_mapper[x] for x in top_node_ids]
+    new_labels = dict(zip(top_node_ids, names))
+    print(new_labels)
+    H.SaveGViz("eigenvector.dot", "Top Eigenvector Nodes", new_labels)
+
+    degree = np.zeros(N, dtype=float)
+    for NI in nodes:
+        node_id = NI.GetId()
+        degree[node_id] = G.GetDegreeCentr(node_id)
+    top_nodes = sorted(list(zip(range(N), degree)), key=lambda x: x[1], reverse=True)[:n]
+    top_node_ids = [node[0] for node in top_nodes]
+    print(top_node_ids)
+    H = G.ConvertSubGraph(snap.TUNGraph, top_node_ids)
+    print(f'nodes {H.GetNodes()} edges {H.GetEdges()}')
+    #H.SaveEdgeList('mygraph.txt')
+    names = [node_mapper[x] for x in top_node_ids]
+    new_labels = dict(zip(top_node_ids, names))
+    print(new_labels)
+    H.SaveGViz("degree.dot", "Top Degree Nodes", new_labels)
+
 #centralityMeasureAnalysisNX(False)
 centralityMeasureAnalysisSNAP()
+#getInducedSubGraph()
